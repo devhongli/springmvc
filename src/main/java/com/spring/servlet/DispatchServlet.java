82be01e30007150ebd923b1d1a9483284e110f03
LiHong <lxh390514899@live.com>
2019-09-18 22:07:04 +0800

    commit

--- src/main/java/com/spring/servlet/DispatchServlet.java
+++ src/main/java/com/spring/servlet/DispatchServlet.java
@@ -6,16 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DispatchServlet extends HttpServlet {
 
-    private Properties p = new Properties();
+    private Properties contextConfig = new Properties();
 
     private Map<String,Object> ioc = new ConcurrentHashMap<String, Object>();
 
+    private List<String> beanNames = new ArrayList<String>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -32,11 +35,31 @@ public class DispatchServlet extends HttpServlet {
         //开始初始化进程
 
         //定位
+        doLoadConfig();
 
         //加载
+        doScanner();
 
         //注册
+        doRegistry();
 
         //自动依赖注入
+        doAutowired();
+
+        //Mvc 会有HnandlerMapping
+
+
+    }
+
+    private void doAutowired() {
+    }
+
+    private void doRegistry() {
+    }
+
+    private void doScanner() {
+    }
+
+    private void doLoadConfig() {
     }
 }
